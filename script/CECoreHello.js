4140fdb2828c0ded06c38c95ee5db6cab61143d0
Cortez Page <cvpage@ucsd.edu>
2019-04-23 15:01:19 -0700

    comments

--- script/CECoreHello.js
+++ script/CECoreHello.js
@@ -3,15 +3,20 @@ export class CECoreHello extends HTMLElement {
 		super();
 		let shadow = this.attachShadow({ mode: "closed" });
 
+    // clone the template to create a fragment
     let coreHelloFrag = document.getElementById("CECoreHelloT").content.cloneNode(true);
     let lang = this.getAttribute("lang");
+    // default to English
     let langVal = lang === null ? "en" : lang.valueOf();
     let greeting = langVal === "es" ? "Hola Mundo" : 
       langVal === "pt" ? "Olá Mundo" : 
       "Hello World";
+    // set paragraph text to the greeting and whatever HTML was originally inside the <ce-core-hello> tag
     coreHelloFrag.querySelector("p").innerHTML = `${greeting} ${this.innerHTML}`;
+    // this isn't necessary, but it gets rid of the original HTML because it is now in the p element
     this.innerHTML = "";
 
+    // cycle through colors of rainbow every 200 ms
     let colorI = 0;
     let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
     if(this.getAttribute("rainbow") !== null) {
@@ -20,6 +25,7 @@ export class CECoreHello extends HTMLElement {
         colorI = (colorI + 1) % colors.length;
       }, 200);
     }
+    
 		shadow.appendChild(coreHelloFrag);
 	}
 }

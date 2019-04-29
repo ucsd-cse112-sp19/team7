e964e08c8d656bb12836a55d85896834f3229785
Yiming Wang <yiw679@ucsd.edu>
2019-04-28 19:21:40 -0700

    Core_Hello Added

---
+++ script/core-hello.js
@@ -0,0 +1,53 @@
+// import { LitElement, html } from 'lit-element';
+
+// class MyComponent extends LitElement {
+//   render() {
+//     var lan = this.getAttribute("lang");
+//     var hws = "Hello World -";
+
+//     if(lan == "English") {
+
+//     }
+//     else if(lan == "Spanish") {
+//         hws = "Hola Mundo -";
+//     }
+//     else if(lan == "Chinese") {
+//         hws = "你好,世界 -";
+//     }
+
+//     return html`
+//       <p>${hws} ${this.textContent} !</p>
+//     `;
+//   }
+// }
+
+// customElements.define('core-hello', MyComponent);
+
+
+class corehello extends HTMLElement {
+  constructor() {
+    super();
+
+    var lan = this.getAttribute("lang");
+    var name = this.innerHTML;
+    const shadowRoot = this.attachShadow({mode: "open"});
+
+
+    var template = document.getElementById("major-template");
+    var clone = document.importNode(template.content, true);
+    var td = clone.querySelector("p");
+      
+    if(lan == "English") {
+      td.textContent = "Hello World - "+name;
+    }
+    else if(lan == "Spanish") {
+      td.textContent = "Hola Mundo - "+name;
+    }
+    else if(lan == "Chinese") {
+      td.textContent = "你好,世界 - "+name;
+    }
+    shadowRoot.appendChild(clone);
+  }
+}
+
+window.customElements.define("core-hello", corehello);
--- script/webcomponents-dan.js
+++ script/webcomponents-dan.js
@@ -1,28 +1,28 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html } from "lit-element";
 
 class WebcomponentsDan extends LitElement { 
     
-    render() {
-        var lang = this.getAttribute('lang');
-        if (lang == 'eng'){
-            return html`
+  render() {
+    var lang = this.getAttribute("lang");
+    if (lang == "eng"){
+      return html`
             <p>Hello World ${this.textContent}</p>
             `;
-        }
-        if (lang == 'spa'){
-            return html`
+    }
+    if (lang == "spa"){
+      return html`
             <p>Hola Mundo ${this.textContent}</p>
             `;
-        }
-        if (lang == 'ger'){
-            return html`
+    }
+    if (lang == "ger"){
+      return html`
             <p>Hallo Welt ${this.textContent}</p>
         `;
-        }
-        return html`
+    }
+    return html`
         <p>Hello World ${this.textContent}</p>
         `;
-    }
+  }
 }
 
-customElements.define('webcomponents-dan', WebcomponentsDan);
+customElements.define("webcomponents-dan", WebcomponentsDan);

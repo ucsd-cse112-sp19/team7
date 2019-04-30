846a8831a857585579235ac045b99f0d6bb11422
EtsuN <etnakaha@ucsd.edu>
2019-04-30 14:32:10 +0800

    attempt to fix deployment issue

--- bash_scripts/deploy_master.sh
+++ bash_scripts/deploy_master.sh
@@ -1,6 +1,1 @@
-sudo gem uninstall travis
-brew install ruby
-ruby -v
-ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-darwin16]
-sudo gem install travis  --no-rdoc --no-ri
-sudo travis version 
+sudo gem update --system

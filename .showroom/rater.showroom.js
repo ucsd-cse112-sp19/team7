b6d0551f5c0dccac2ff795e93018339ea41c96f8
cmshroe <cmschroe@ucsd.edu>
2019-05-15 15:35:38 -0700

    added unit tests for rate component

---
+++ .showroom/rater.showroom.js
@@ -0,0 +1,10 @@
+export default {
+  component: 'rater-r',
+  path: '/script/Rater.js',
+  events: ['clicked'],
+  attributes: {
+    'author': 'stupidEtsu',
+    'des': 'Etsu is dumb',
+    'max': '6'
+  },    
+}

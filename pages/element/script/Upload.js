75420da04c5d177c90b27acef5dfb68e5ccdc662
DanNguyen8189 <dannguyen8189@gmail.com>
2019-05-29 22:10:12 -0700

    firebase stuff

--- pages/element/script/Upload.js
+++ pages/element/script/Upload.js
@@ -18,6 +18,45 @@
  * Cloning contents from a &lt;template&gt; element is more performant
  * than using innerHTML because it avoids addtional HTML parse costs.
  */
+
+      // Your web app's Firebase configuration
+      var firebaseConfig = {
+        apiKey: "AIzaSyAWQ3Vi-7CTKYVgS1yQ7WHA7S0vShJCgZs",
+        authDomain: "team7-element.firebaseapp.com",
+        databaseURL: "https://team7-element.firebaseio.com",
+        projectId: "team7-element",
+        storageBucket: "team7-element.appspot.com",
+        messagingSenderId: "860166313157",
+        appId: "1:860166313157:web:59ab005177ad2f01"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+const storageService = firebase.database();
+const storageRef = storageService.ref();
+
+
+document.querySelector('.file-select').addEventListener('change', handleFileUploadChange);
+document.querySelector('.file-submit').addEventListener('click', handleFileUploadSubmit);
+
+let selectedFile;
+function handleFileUploadChange(e){
+  selectedFile = e.target.files[0];
+}
+
+function handleFileUploadSubmit(e) {
+  const uploadTask = storageRef.child(`images/${selectedFile.name}`).put(selectedFile); //create a child directory called images, and place the file inside this directory
+  uploadTask.on('state_changed', (snapshot) => {
+  // Observe state change events such as progress, pause, and resume
+  }, (error) => {
+    // Handle unsuccessful uploads
+    console.log(error);
+  }, () => {
+     // Do something once upload is complete
+     console.log('success');
+  });
+}
+
 const template = document.createElement("template");
 template.innerHTML = `
   <style>

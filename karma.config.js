cdec64d79309a6047f8dd677f564ec5b961e9213
Li-An Yang <luke831215@gmail.com>
2019-05-31 23:24:12 -0700

    fix require path for webpack in karma config

--- karma.config.js
+++ karma.config.js
@@ -63,6 +63,6 @@ module.exports = function (config) {
         // Concurrency level: how many browser should be started simultaneous
         concurrency: Infinity,
        
-        webpack: require("config/webpack.dev"),
+        webpack: require("./config/webpack.dev"),
     })
 }

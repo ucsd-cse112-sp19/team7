1045197aae547541e2a2a0228c88d36db7652628
Cortez Page <cvpage@ucsd.edu>
2019-06-02 15:38:50 -0700

    travis npm

---
+++ index.js
@@ -0,0 +1,4 @@
+import { Checkbox } from "./pages/element/script/Checkbox.js";
+import { Rater } from "./pages/element/script/Rater.js";
+Checkbox.toString();
+Rater.toString();
--- pages/element/script/index.js
+++
@@ -1,4 +0,0 @@
-import { Checkbox } from "./Checkbox.js";
-import { Rater } from "./Rater.js";
-Checkbox.toString();
-Rater.toString();

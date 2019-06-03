d50ec7ee6e84cc2282de7eefcc3ca34eee5d7658
Cortez Page <cvpage@ucsd.edu>
2019-06-02 17:52:30 -0700

    only publish when push to master

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { Checkbox } from "./pages/element/script/Checkbox.js";
 import { Rater } from "./pages/element/script/Rater.js";
-Checkbox.toString();
+Checkbox.toString(); // to pass linter
 Rater.toString();

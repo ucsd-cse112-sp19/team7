521a6c8b52bce1bc5ec1b2bc018c2c3410040a24
cmshroe <cmschroe@ucsd.edu>
2019-06-02 17:00:50 -0700

    trying snapshot

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
 import { storiesOf } from '@storybook/html';
 
 storiesOf('Button', module)
-  .add('with text', () => '<button class="btn">Hello World</button>');
+  .add('with text', () => '<button class="btn">Hello World!</button>');

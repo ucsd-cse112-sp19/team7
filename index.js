b8f2ec5fa9c07ae5d870e7259bb2e6d0a4ed8cff
cortezpage <cvpage@ucsd.edu>
2019-06-04 14:17:45 -0700

    added new comps to packag

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { Checkbox } from "./pages/element/script/Checkbox.js";
 import { Rater } from "./pages/element/script/Rater.js";
+import { Upload } from "./pages/element/script/Upload.js";
+import { Comment } from "./pages/element/script/Comment.js";
 Checkbox.toString(); // to pass linter
 Rater.toString();
+Upload.toString();
+Comment.toString();

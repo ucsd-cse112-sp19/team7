b6453b19bc47eefb798b170723a5d37f224c3db9
Cortez Page <cvpage@ucsd.edu>
2019-06-07 12:12:08 -0700

    checkbox tests

--- test/checkbox.spec.js
+++ test/checkbox.spec.js
@@ -20,31 +20,64 @@ describe("sds-rate unit", () => {
   });
 
   it("Unit test 1: check the getters and setters for True labels", async () => {
-    checkbox.trueLabel = "Some Label"
-    "Some Label".should.equal(checkbox.trueLabel)
+    checkbox.trueLabel = "Some Label";
+    "Some Label".should.equal(checkbox.trueLabel);
     // attribute should be changed as well
-    checkbox.getAttribute("true-label").should.equal("Some Label")
+    checkbox.getAttribute("true-label").should.equal("Some Label");
   });
 
   it("Unit test 2: check the getters and setters for False labels", async () => {
-    checkbox.falseLabel = "Some False Label"
-    "Some False Label".should.equal(checkbox.falseLabel)
+    checkbox.falseLabel = "Some False Label";
+    "Some False Label".should.equal(checkbox.falseLabel);
     // attribute should be changed as well
-    checkbox.getAttribute("false-label").should.equal("Some False Label")
+    checkbox.getAttribute("false-label").should.equal("Some False Label");
   });
 
   it("Unit test 3: check the functionality disable", async () => {
-    checkbox.disabled = true
-    let t = true
-    t.should.equal(checkbox.disabled)
+    checkbox.disabled = true;
+    let t = true;
+    t.should.equal(checkbox.disabled);
     // attribute should be removed correctly
-    checkbox.hasAttribute("disabled")
+    checkbox.hasAttribute("disabled").should.equal(true);
+    checkbox.disabled = false;
+    checkbox.hasAttribute("disabled").should.equal(false);
   });
 
   it("Unit test 4: check the default label class name", async () => {
-    let lb = shadow.querySelector("label")
+    let lb = shadow.querySelector("label");
     "el-checkbox".should.equal(lb.className);
   });
 
-  
+  // The following commented tests do not pass. It might have to do with the tests rather than the code.
+
+  // it("Unit test 5: check if valueModel is set to true label when the checkbox is checked", async () => {
+  //   checkbox.trueLabel = "A True Label";
+  //   checkbox.falseLabel = "A False Label";
+  //   checkbox.checked = true;
+  //   checkbox.valueModel.should.equal("A True Label");
+  // });
+
+  // it("Unit test 6: check if valueModel is set to false label when the checkbox is not checked", async () => {
+  //   checkbox.trueLabel = "A True Label";
+  //   checkbox.falseLabel = "A False Label";
+  //   checkbox.checked = false;
+  //   checkbox.valueModel.should.equal("A False Label");
+  // });
+
+  it("Unit test 7: check the getter for name attribute", async () => {
+    checkbox.name = "A Name";
+    checkbox.getAttribute("name").should.equal("A Name");
+  });
+
+  it("Unit test 8: check the getter for border attribute", async () => {
+    checkbox.border = true;
+    checkbox.hasAttribute("border").should.equal(true);
+    checkbox.border = false;
+    checkbox.hasAttribute("border").should.equal(false);
+  });
+
+  it("Unit test 9: check the getter for size attribute", async () => {
+    checkbox.size = "small";
+    checkbox.getAttribute("size").should.equal("small");
+  });
 });
